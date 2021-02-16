943bb992f7bfae3b25c910eaafd2cdd5875f5362
maximillian.wiesner@colorado.edu <maximillian.wiesner@colorado.edu>
2021-02-15 18:23:15 -0700

    added secant method

--- Newton_Raphson_Method.m
+++ Newton_Raphson_Method.m
@@ -3,10 +3,12 @@ function p = Newton_Raphson_Method(f, p_0, N, e)
     % 2/15/21
     % 
     % This function iterates using the Newton Raphson method to 
-    % to find a root on the given continuous funciton 
-    %
+    % to find a root on the given continuous funciton, can be though 
+    % of as successive tangent approximation.
+    % 
     % --- Requirements --- 
-    % must declare x prior, ie. 'syms x' then call the function
+    % must declare x prior, ie. 'syms x' then call the function, p_0
+    % cannot be such that f'(p_0) = 0.
     %
     % --- Parameters ---
     % f   : the function to find the root on
@@ -30,8 +32,7 @@ function p = Newton_Raphson_Method(f, p_0, N, e)
     i = 1;
     while i <= N
         p = p_0 - f(p_0)/df(p_0);
-        fprintf("\np_%d = %-5.10f", i, p);
-        
+        fprintf("\np_%d = %-5.10f", i, p);        
         if abs(p - p_0) < e
             fprintf("\n\np found after %d iterations.", i);
             return;
@@ -43,4 +44,18 @@ function p = Newton_Raphson_Method(f, p_0, N, e)
     fprintf("\nThe method failed after %d iterations. ", i);
     return;
 end
+
+% example 
+% >> sysm x
+% >> f = x^3 - 0.165*x + 4*(10^-4)
+% >> ans = Newton_Raphson_Method(f,0,10,10^(-5))
+
+% output 
+% p_1 = 0.0024242424
+% p_2 = 0.0024243288
+%
+% p found after 2 iterations.
+% ans =
+%
+%   0.0024
 
